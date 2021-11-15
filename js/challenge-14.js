55366520c183a7502d9f894b788b37d5876479f4
Oscar Sierra <oscarsierrafotografias@gmail.com>
2021-11-14 23:21:13 -0300

    subiendo challenge 14

---
+++ js/challenge-14.js
@@ -0,0 +1,10 @@
+let a = Number(prompt("Ingrese Un Numnero"));
+let b = Number(prompt("Ingrese otro Numero"));
+
+if (a > b){
+    alert("el 1er Numero es Mayor")
+}else if(a == b){
+    alert("Los Dos numeros Son  Iguales")
+}else{
+    alert("el 1er Numero es Menor")
+}
